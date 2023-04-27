9a78f3a14cee08c647181dfcc4f7800e5bae8eb9
aleksandre2001 <mister.verulashvili@gmail.com>
2023-04-28 00:31:57 +0400

    movie-web

---
+++ index.js
@@ -0,0 +1,9 @@
+// fetch("http://www.omdbapi.com/apikey=1fb0a3cb")
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+    
+
+    // fetch("http://www.omdbapi.com/?apikey=1fb0a3cb")
+    // .then(res => res.json())
+    // .then(data => console.log(data))
---
+++ readme.md
@@ -0,0 +1,4 @@
+1. make  basic htmls 
+2. make a basic  css
+3. look in to api working, what this returns
+4 async functions, callbacks, render in html div, inner html, filter, map, array
