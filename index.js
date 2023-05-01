2812317db19396bf806ed86930a37dd4f0a32546
aleksandre2001 <mister.verulashvili@gmail.com>
2023-05-01 23:04:32 +0400

    3

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-fetch("https://www.omdbapi.com/?s=cars&apikey=1fb0a3cb")
-    .then(res => res.json())
-    .then(data => {
-        const sumerr = data
-        console.log(sumerr)
-    })
+// fetch("https://www.omdbapi.com/?s=cars&apikey=1fb0a3cb")
+//     .then(res => res.json())
+//     .then(data => {
+//         const sumerr = data
+//         console.log(sumerr)
+//     })
 
-    
+
+
+
+class Movie{
+    constructor(data){
+        Object.assign(this, data)
+       
+    }
+    consoling(){
+        return this.name
+    }
+}
+
+const someObj ={
+    name: "aleko"
+}
+
+const newObje = new Movie(someObj)
+
+console.log(newObje.consoling())
