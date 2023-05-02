8fb0acc4e716fc2c65873ba2420479a587663df1
aleksandre2001 <mister.verulashvili@gmail.com>
2023-05-02 09:56:31 +0400

    3

---
+++ data.js
@@ -0,0 +1,6 @@
+export class Movie {
+    constructor(data){
+        Object.assign(this, data)
+        
+    }
+}
--- index.js
+++ index.js
@@ -1,27 +1,70 @@
-// fetch("https://www.omdbapi.com/?s=cars&apikey=1fb0a3cb")
-//     .then(res => res.json())
-//     .then(data => {
-//         const sumerr = data
-//         console.log(sumerr)
-//     })
+let responseArr = []
+let movieArr = []
 
 
 
+// document.getElementById('submit').addEventListener('submit', function(){
+    
+// })
 
-class Movie{
-    constructor(data){
-        Object.assign(this, data)
-       
-    }
-    consoling(){
-        return this.name
-    }
-}
+const searchForm = document.getElementById('search-box')
+const inputField = document.getElementById('input')
 
-const someObj ={
-    name: "aleko"
-}
+searchForm.addEventListener("submit", function(){    
+        fetch(`https://www.omdbapi.com/?s=${inputField.value}&apikey=1fb0a3cb`)
+            .then(res => res.json())
+            .then(data => {
+                responseArr = data.Search
+                if(data.Search){createMovieArr()}
+                console.log(movieArr)
+                if(movieArr){render(movieArr)}
+            })
 
-const newObje = new Movie(someObj)
+        function render(array){
+            array.forEach(movie => {
+                document.querySelector("#movies-wrapper").innerHTML += `
+                    <div class="movie">
+                        <div class= "poster" style=" background-image: url("${movie.Poster})"; "></div>
+                        <div class = "content"> 
+                        <div class="title-data">
+                        <span class="title">${movie.Title} </span>  
+                        <span class="rating-data"> <img src="./images/star-icon.png"> 8.1</span>
+                    </div>
+                    
+                    <div class="about1">
+                        <span> 117min</span>
+                        <span>Animation, Adventure</span>
+                        <span> 
+                        <a href="#">
+                            <button><img src="./images/plus-icon.png"/> Watchlist</button>
+                        </a>
+                        </span>
+                    </div>
+                    <div class="about2"><p>A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p></div>
+                    </div>
+                </div>            
+                `
+            })
+        }
+        
+        function createMovieArr(){
+            responseArr.forEach(movieData => {
+                let movie = new Movie(movieData)
+                // console.log(movieData)
+                movieArr.push(movie)
+            });
+        
+        }
 
-console.log(newObje.consoling())
+
+        class Movie{
+        constructor(data){
+            Object.assign(this, data)
+        }
+        }
+
+})
+    
+
+
+
